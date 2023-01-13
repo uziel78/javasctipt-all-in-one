09ea1bfc8c395109c6d9e3ca5b30fe46d635cec0
uziel78 <rune.husa78@gmail.com>
2023-01-13 23:45:12 +0100

    just some useful String methods

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log('Hello')
+// Strings
+
+const myVariable = 'Mathematics';
+
+// The Length Property
+
+// console.log('myVariable'.length);
+
+// String methids
+// console.log(myVariable.charAt(0));
+// console.log(myVariable.indexOf('Mat'));
+// console.log(myVariable.slice(5, 8));
+// console.log(myVariable.toUpperCase());
+// console.log(myVariable.includes('div'));
+// console.log(myVariable.split('e'));
+// console.log(myVariable.split('e'));
