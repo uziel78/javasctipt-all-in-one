a316609cb4a6982a2a651e638a7f2429739add67
uziel78 <rune.husa78@gmail.com>
2023-01-14 12:34:18 +0100

    String methods examples

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
-// Strings
+// Numbers
 
-const myVariable = 'Mathematics';
+// An integer is a whole number.
 
-// The Length Property
+const myNumber = 42;
 
-// console.log('myVariable'.length);
+const myFloat = 42.01;
 
-// String methids
-// console.log(myVariable.charAt(0));
-// console.log(myVariable.indexOf('Mat'));
-// console.log(myVariable.slice(5, 8));
-// console.log(myVariable.toUpperCase());
-// console.log(myVariable.includes('div'));
-// console.log(myVariable.split('e'));
-// console.log(myVariable.split('e'));
+const myString = '42.123abc';
+
+//console.log(myNumber === myFloat);
+
+// change string to number with Number fucntion
+// console.log(Number(myString) + 3);
+
+// The Number.isInteger method determines if the passed value is an integer.
+//console.log(Number.isInteger(myNumber));
+
+// The Number.parseFloat() method parses an argument and returns a floating point number.
+// If a number cannot be pardsed from the argument, it returns NaN.
+//console.log(Number.parseFloat(myString));
+
+// Rounds up decimals to (x). Returns String Date, not number.
+//console.log(myFloat.toFixed(2));
+
+//converts argument to Integer, removes non.numeric characters and converts other data.ypes to numbers when possible
+//console.log(Number.parseInt(myString));
+
+// converts data to String type
+//console.log(typeof myFloat.toString());
+
+// The Number .isNaN() method determines wheter the passed value is NaN AND is type number:
+// Number.isNaN();
+
+// console.log(Number('Rune'));
+// console.log(isNaN('Rune'));
