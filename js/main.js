6306a7d83299436b0a090503775337211334cd76
uziel78 <rune.husa78@gmail.com>
2023-01-15 19:00:12 +0100

    Refactored Rock, Paper, Scissors with while loop

--- js/main.js
+++ js/main.js
@@ -1,105 +1,48 @@
-// ----- Arrays ----- //
+// ----- Refactor Game with Arrays ----- //
 
-//const myArray = [];
+// ----- Rock, Paper Scissors Refactored with while Loop (working)
 
-//add elements to an array
+let playGame = confirm('Shall we play rock, paper or scissors?');
+if (playGame) {
+  //play
+  while (playGame) {
+    const playerChoice = prompt('Please enter Rock, Paper or Scissors.');
+    if (playerChoice || playerChoice === '') {
+      const playerOne = playerChoice.trim().toLowerCase();
+      if (
+        playerOne === 'rock' ||
+        playerOne === 'paper' ||
+        playerOne === 'scissors'
+      ) {
+        const computerChoice = Math.floor(Math.random() * 3);
+        const rpsArray = ['rock', 'paper', 'scissors'];
+        const computer = rpsArray[computerChoice];
 
-// myArray[0] = 'Rune';
-// myArray[1] = 1001;
-// myArray[2] = false;
+        const result =
+          playerOne === computer
+            ? 'Tie Game!'
+            : playerOne === 'rock' && computer === 'paper'
+            ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+            : playerOne === 'paper' && computer === 'scissors'
+            ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+            : playerOne === 'scissors' && computer === 'rock'
+            ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+            : `playerOne: ${playerOne}\nComputer: ${computer}\nPlayer One wins!`;
+        alert(result);
 
-// ----- refer to array
-//console.log(myArray);
-
-//----- length property
-//console.log(myArray.length);
-
-// ----- last element
-//console.log(myArray[myArray.length - 1]);
-
-// ----- any one item
-//console.log(myArray[1]);
-
-// ----- add to array
-//myArray.push('Christmas');
-//console.log(myArray);
-
-// ----- remove last item from array
-//myArray.pop();
-//console.log(myArray);
-
-// ----- take out last item and assiign to variable
-//const lastItem = myArray.pop();
-//console.log(lastItem);
-
-// ----- add first item of array
-//myArray.unshift(42);
-//console.log(myArray);
-
-//the same below also goes for push
-// const newLength = myArray.unshift(42);
-// console.log(newLength);
-
-// ----- remove from front of the Array
-// myArray.shift();
-// console.log(myArray);
-
-//take out of array and assign to variable
-// const firstItem = myArray.shift();
-// console.log(firstItem);
-// console.log(myArray); //verify removal
-
-// ----- remove item from middle of array (leaves empty space in array with undefined data!)
-// delete myArray[1];
-// console.log(myArray);
-
-// ----- remove and replace item in array (the corerct way). Surgical removal of one or more coherent positions in an Array
-//myArray.splice(1, 1); //remove one element from the 1 position
-//console.log(myArray);
-
-// can be used to replace values:
-//myArray.splice(1, 1, 42); //1st position, 1 item, replaced with number 42
-//myArray.splice(1, 0, 42); // fistposition, add without deleting (0), number 42
-//console.log(myArray);
-
-// ----- part 2 -----
-
-//slice method example
-//const myArray = ['A', 'B', 'C', 'D', 'E', 'F'];
-//console.log(myArray);
-
-//const newArray = myArray.slice(2, 5); //new array starts at position 2, ends at position 5 (not including position 5)
-//onsole.log(newArray);
-
-// ----- reverse method
-//myArray.reverse(); //does not create a new array
-//console.log(myArray);
-
-// ----- join method
-//myArray.join();
-//const newString = myArray.join(); //all elements in array becomes one String
-//console.log(newString);
-
-// ----- split method (string method, not array method). Reverses the join method above in this case.
-//const newArray = newString.split(','); //creates new array!
-//console.log(newArray);
-
-// ----- part 3 -----
-
-// ----- combining arrays with concat method
-const myArrayA = ['A', 'B', 'C'];
-const myArrayB = ['D', 'E', 'F'];
-
-//const newArray = myArrayB.concat(myArrayA);
-//console.log(newArray);
-
-// ----- The Spread operator (newer way of doing the above)
-const newArray = [...myArrayA, ...myArrayB];
-console.log(newArray);
-//spread operators (...) takes the individual elements from the
-// two above arrays and combines them in a new array, instead of making two nested arrays (see below)
-const nestedArrays = [myArrayA, myArrayB];
-console.log(nestedArrays);
-
-// ----- how to log deeper into nested arrays
-console.log(nestedArrays[0][1]); //logs letter B from the fist nested array and position 1 in that array
+        playGame = confirm('Play Again?');
+        if (!playGame) alert('Ok, thanks for playing');
+        continue;
+      } else {
+        alert("you didn't enter rock, paper or scissors.");
+        continue;
+      }
+    } else {
+      alert('I guess you changed your mind. Maybe next time.');
+      break;
+    }
+  }
+} else {
+  console.log("You didn't enter your name");
+  alert('ok, maybe next time.');
+}
