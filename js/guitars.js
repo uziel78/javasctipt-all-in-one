f4993e2f6a862b7b62e156f2d366cb03d4a8bda0
uziel78 <rune.husa78@gmail.com>
2023-01-16 22:34:46 +0100

    guitars.js added back

---
+++ js/guitars.js
@@ -0,0 +1,14 @@
+export default function playGuitar() {
+  return 'Playing Guitar';
+}
+
+export const shredding = () => {
+  return 'Shredding Some Licks!';
+};
+
+export const plucking = () => {
+  return 'Plucking the strings...';
+};
+
+//export default playGuitar;
+// export { shredding, plucking };
